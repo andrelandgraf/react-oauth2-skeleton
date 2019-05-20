e698d5a287a2494bc7e7ef0858c39fa0e7600234
Andre Landgraf <anla@netlight.com>
2019-05-20 23:59:25 +0200

    cleanup

--- src/js/services/oAuthService.js
+++ src/js/services/oAuthService.js
@@ -26,23 +26,6 @@ export const getOAuthHeader = ( clientID, clientSecret ) => {
 export const postAuthRequest = ( params, data, headers ) => axios
     .post( API + AUTH_ENDPOINT + params, data, { headers } );
 
-// probably not needed, as alexa will do this for herself and we use grant_type password for react
-// first type retrieval of auth token
-// const getAuthToken = ( authCode ) => {
-//     // afer getting redirected back to our page,
-//     // we can access the authCode get-parameter and call the authentification
-//     // const authCode = req.query.code;
-//     // getAuthToken(authCode);
-//     const headers = getOAuthHeader();
-//     const params = `?grant_type=${ GRANT_TYPES.AUTH_CODE }&code=${ authCode }&redirect_uri=http://localhost:3000/`;
-//     const data = {
-//         grant_type: GRANT_TYPES.AUTH_CODE,
-//         code: authCode,
-//         redirect_uri: 'http://localhost:3000/',
-//     };
-//     postAuthRequest( params, data, headers );
-// };
-
 // refresh authToken
 export const refreshAuthToken = ( resolve ) => {
     const headers = getOAuthHeader();
