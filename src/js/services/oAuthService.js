1cfbfc15c21dd1e3c26b1b104de0504804af34fc
Andre Landgraf <anla@netlight.com>
2019-06-11 14:12:16 +0200

    debugging auth flow

--- src/js/services/oAuthService.js
+++ src/js/services/oAuthService.js
@@ -47,8 +47,7 @@ export const postAuthRequest = ( data, headers ) => axios
     .catch( err => isNetworkError( err ) && throwServerNotReachableError() );
 
 export const getAuthorizeCode = ( clientId, state, redirectUri ) => {
-    const params = `?client_id=${ clientId }&response_type=code&state=${ state }&redirect_uri="${ redirectUri }"`;
-    console.log( params );
+    const params = `?client_id=${ clientId }&response_type=code&state=${ state }&redirect_uri=${ redirectUri }`;
     return fetch( `${ API }${ AUTHORIZE_ENDPOINT }${ params }`, { headers: getCodeHeaders() } );
 };
 
